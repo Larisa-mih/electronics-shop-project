ff520f19b71e02e07c3c8de429131ec8f93d6f95
larisa <larisa.mih2004@gmail.com>
2024-01-28 04:01:20 +0300

    modified main.py for homework-6

--- homework-6/main.py
+++ homework-6/main.py
@@ -1,10 +1,11 @@
+from config import OPERATIONS_PATH
 from src.item import Item
 
 if __name__ == '__main__':
     # Файл items.csv отсутствует.
-    Item.instantiate_from_csv()
+    Item.instantiate_from_csv(OPERATIONS_PATH)
     # FileNotFoundError: Отсутствует файл item.csv
 
     # В файле items.csv удалена последняя колонка.
-    Item.instantiate_from_csv()
+    Item.instantiate_from_csv(OPERATIONS_PATH)
     # InstantiateCSVError: Файл item.csv поврежден
